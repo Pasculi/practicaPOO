2225ccd0e34e0c515baf594944d4132b2fba1d2a
Rodrigo Turpie <jsepulveda_turpie@hotmail.com>
2024-01-29 07:46:50 -0300

    feat: Se realizan las metodos de _handleOpenPopup, _handleClosePopup, _setEventListeners

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const items = [
   },
 ];
 
-class Card {
+/* class Card {
   constructor(title, description, price, image) {
     this._title = title;
     this._description = description;
@@ -44,5 +44,55 @@ class Card {
 items.forEach(item => {
   const section = document.querySelector('.card-list__items')
   const card = new Card(item.title, item.description, item.price, item.image)
+  section.append(card.generateCard())
+})
+ */
+//¿Cómo se pasa un objeto al método constructor()?
+
+const popupElement = document.querySelector(".popup");
+const popupImage = document.querySelector(".popup__image");
+class Card {
+  constructor(data, cardSelector) {
+    this._title = data.title;
+    this._description = data.description;
+    this._price = data.price;
+    this._image = data.image;
+    this._cardSelector = cardSelector
+  }
+  _getTemplate() {
+    const template = document.querySelector(this._cardSelector).content
+    const card = template.querySelector('.card').cloneNode(true);
+    return card;
+  }
+  generateCard() {
+    this._element = this._getTemplate();
+    this.setEventListeners();
+    this._element.querySelector('.card__image').style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector('.card__title').textContent = this._title;
+    this._element.querySelector('.card__info').textContent = this._description;
+    this._element.querySelector('.card__price-property').textContent = this._price;
+    return this._element;
+  }
+  _handleOpenPopup(){
+    this._imagen.setAttribute = ('src', popupImage);
+    popupElement.classList.add('.popup__is-opened')
+  }
+  _handleClosePopup(){
+    this._imagen.setAttribute = ('src', '');
+    popupElement.classList.remove('.popup__is-opened')
+  }
+  _setEventListeners(){
+    this._element.addEventlistener('click',() =>{
+      this._handleOpenPopup();
+    })
+    popupCloseButton.addEventlistener('click', ()=>{
+      this._handleClosePopup()
+    })
+  }
+}
+
+items.forEach(item => {
+  const section = document.querySelector('.card-list__items')
+  const card = new Card(item, ".horizontal-card")
   section.append(card.generateCard())
 })
